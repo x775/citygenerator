b33ebf8c13a70c856b7bb050b4715829755ea9a8
pverge <pverge15@student.aau.dk>
2019-08-26 21:18:53 +1200

    updated config implementation. still WIP
    Co-authored-by: x775 <x775@users.noreply.github.com>

--- python/city_generation/config_loader.py
+++ python/city_generation/config_loader.py
@@ -1,16 +1,54 @@
 import os
 import json
+import numpy as np
+from input_setup import *
+from roadmap_generation.vertex import Vertex
 
 class ConfigLoader:
     def __init__(self, config=None):
         directory = os.getcwd()
-
+        
         try:
             with open(directory + "/configs/" + config + ".json", "r") as config_file:
                 for key, value in json.loads(config_file).items():
                     setattr(self, key, value["value"])
         except FileNotFoundError:
             print("Incorrect or missing config file!")
+            #break
+
+        # Call all generation functions
+        self.init_road_network()
+
+
+    def init_road_network(self):
 
         self.maxLength = max(self.radiallMax, self.gridlMax, self.organiclMax,
                             self.minor_roadlMax, self.seedlMax)
+        
+        # Create initial list of starter Vertex-objects as supplied in config.
+        self.axiom = [Vertex(np.array([coordinate[0], coordinate[1]])) for coordinate in self.axiom]
+
+        # Parse road rule map and population density map.
+        self.road_rules = parse_image(self.road_rule_path)
+        self.population_density = parse_image(self.population_density_path)
+
+        # Find city centers based on population density
+        # self.city_centers = find_legend_color_coordinates(self.population_density, self.population_density_legend)
+
+        # Find radial rule centers
+        # TODO: currently only works for a single radial pattern
+        self.radial_rule_centers = find_radial_rule_centers(self.road_rules, self.road_rules_legend)
+
+
+        
+
+
+"""
+Config file should contain
+
+* one config file per city; contains all input
+* list of relevant legends for input images
+* list of population density segmentations (last in list = highest density)
+
+
+"""
--- python/city_generation/input_setup.py
+++ python/city_generation/input_setup.py
@@ -1,9 +1,45 @@
 import numpy as np
 from PIL import Image
 
+
+# INPUT: name of image file
+# OUTPUT: 3d array of color values (r,g,b,a)
 def parse_image(filename):
     # We open the supplied image and convert it to a numpy array.
-    # dtype is inferred as uint8, and the output is an array of 
+    # dtype is inferred as uint8, and the output is an array of
     # lists where each parent list corresponds to a row of pixel
     # values. All pixels contain [r, g, b, a]-values.
     return np.asarray(Image.open(filename))
+
+
+# INPUT: 3d array of color values (r,g,b,a)
+#        color to find coordinates of (r,g,b,a)
+# OUTPUT: 2d array of coordinates of pixels with given color
+def find_legend_color_coordinates(image_arr, legend_color):
+    # Given an array of colour values, and a specific legend color,
+    # we create and subsequently zip a 2-tuple of arrays containing 
+    # indices in the first and second dimension corresponding to the 
+    # pixel values of the colour matching the legend.
+    indices = np.where(np.all(image_arr == legend_color, axis=-1))
+    # Convert iterator to list to array before returning.
+    return np.array(list(zip(indices[0], indices[1]))) 
+
+
+# INPUT: 2d array of coordinates
+# OUTPUT: x and y tuple for centroid
+def find_coordinates_centroid(coordinates):
+    # Given an array of coordinates, we find the centroid
+    # of the coordinates and returns them as a tuple.
+    coordinate_length = coordinates.shape[0]
+    coordinate_x_sum = np.sum(coordinates[:, 0])
+    coordinate_y_sum = np.sum(coordinates[:, 1])
+    return (coordinate_x_sum / coordinate_length, coordinate_y_sum / coordinate_length)
+
+
+def find_radial_rule_centers(image_arr, legend_color):
+    indices = find_legend_color_coordinates(image_arr, legend_color)
+    return find_coordinates_centroid(indices)
+
+
+
+# TODO: implement clustering for multiple clusters of same legend color
